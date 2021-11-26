403c558a4f506a0734d12153d87162af8290607d
Antony Deepak Thomas <antonydeepak@gmail.com>
2021-11-26 10:57:05 -0800

    f# setup

---
+++ f#/1.fs
@@ -0,0 +1,7 @@
+open Common
+
+module part1 =
+    let solve (input: List<string>) = input
+
+let input = Common.readIn
+input |> part1.solve |> Common.writeOut
---
+++ f#/README.md
@@ -0,0 +1,2 @@
+This runs in dotnet 6.
+Run a solution using `dotnet run <day>.fs` as in `dotnet run 1.fs`
