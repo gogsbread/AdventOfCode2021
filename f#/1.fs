30c1278a9a943bcf3b504cb8aa4bff5d3e6a6665
Antony Deepak Thomas <antonydeepak@gmail.com>
2021-12-05 21:22:03 -0800

    1 fs

--- f#/1.fs
+++ f#/1.fs
@@ -1,7 +1,11 @@
 open Common
 
 module part1 =
-    let solve (input: List<string>) = input
+    let solve (input: List<string>) =
+        input
+        |> List.pairwise
+        |> List.filter (fun (f, l) -> l > f)
+        |> List.length
 
 let input = Common.readIn
 input |> part1.solve |> Common.writeOut
